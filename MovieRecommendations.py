bb15c0cd50a7299b65392898e954d0057711d5cf
hiroshige.e <hiroshige.e@husky.neu.edu>
2018-09-19 22:17:48 -0400

    json files to represent data

---
+++ MovieRecommendations.py
@@ -0,0 +1,27 @@
+import json
+from typing import Dict, Any
+
+import numpy as np
+import pandas
+
+# new movie recommendation script
+
+def jsonify(movie_file, ratings_file):
+    # movie_info: first column is movie id, second is movie title, third is genre
+    movie_info = pandas.io.parsers.read_csv(movie_file, delimiter=',', dtype=None, encoding=None).values
+    # ratings: first column is user id, second is movie id, third is rating
+    ratings = pandas.io.parsers.read_csv(ratings_file, delimiter=',', dtype=None, encoding=None).values
+    # userMovie = input("Enter a movie: ")
+    movie_dict = {}
+    for i in range(0, len(movie_info)):
+        title: str = movie_info[i][1]
+        movie_dict[movie_info[i][0]] = {"title": title[0: -7], "genres": movie_info[i][2]}
+
+    with open('movies.json', 'w') as file:
+        file.write(json.dumps(movie_dict))
+
+    ratings_dict = {}
+
+    for i in range(0, len(ratings)):
+        critic = ratings_dict.get(ratings[i][0], {})
+        critic[ratings[i][1]] = ratings[i][2]
--- movieratingproject.py
+++ movieratingproject.py
@@ -1,7 +1,11 @@
+from typing import List, Any
+
 import numpy as np
 
-movienamekey = np.genfromtxt('movienamekey.csv', delimiter=',', dtype=None)
-data = np.genfromtxt('updatedmoviedata.csv', delimiter=',', dtype=None)
+# movie id, title
+movieInfo = np.genfromtxt('movienamekey.csv', delimiter=',', encoding=None, dtype=None)
+# movie id, critic, rating
+ratings = np.genfromtxt('updatedmoviedata.csv', delimiter=',', encoding=None, dtype=None)
 
 # TODO
 # 1. Parse data to be much smaller
@@ -12,53 +16,55 @@ data = np.genfromtxt('updatedmoviedata.csv', delimiter=',', dtype=None)
 #
 
 
-movienums = len(movienamekey)
-ldata = len(data)
 movie = 0
 
+# FIND ID OF ENTERED MOVIE
 # Takes movie name input
-moviename = raw_input("Enter movie name: ")
-for i in range(len(movienamekey)):
-  if moviename == movienamekey[i][1]:
-    movie = movienamekey[i][0]
+movieName = input("Enter moaaavie name: ")
+for i in range(len(movieInfo)):
+    if movieName == movieInfo[i][1]:
+        movie = movieInfo[i][0]
 
+# ADD ALL RATINGS OF MOVIE TO R
 # list of ratings
 r = []
-for i in range(ldata):
-  if data[i][0] == movie:
-    r.append(data[i][2])
+for i in range(len(ratings)):
+    if ratings[i][0] == movie:
+        r.append(ratings[i][2])
 
+# ADD ALL MOVIE IaDS INTO AN ARRAY KEY
 # movie number key in array
-key = []
-for i in range(movienums):
-  key.append(movienamekey[i][0])
+movieIds = []
+for i in range(len(movieInfo)):
+    movieIds.append(movieInfo[i][0])
 
 # calculates distances
-q = len(key)
+q = len(movieIds)
 distances = []
 for i in range(q):
-  compare = []
-  dif = []
-  for l in range(ldata):
-    if key[i] == data[l][0] and key[i] != movie:
-      compare.append(data[l][2])
-      if len(r) == len(compare):
-        for k in range(len(r)):
-          dif.append((r[k] - compare[k]) ** 2)
-        distances.append(sum(dif) ** 0.5)
+    compare = []
+    dif = []
+    for l in range(len(ratings)):
+
+        if movieIds[i] == ratings[l][0] and movieIds[i] != movie:
+            compare.append(ratings[l][2])
+            if len(r) == len(compare):
+                for k in range(len(r)):
+                    dif.append((r[k] - compare[k]) ** 2)
+                distances.append(sum(dif) ** 0.5)
 
 # inserts zero into distances
-zero = key.index(movie)
+zero = movieIds.index(movie)
 distances.insert(zero, 0)
 
 # creates array with number of movies
 last = []
-for i in range(movienums):
-  last.append(i)
+for i in range(len(movieInfo)):
+    last.append(i)
 
 # sorts distances and last in same way
 distances, last = zip(*sorted(zip(distances, last)))
 
 # prints rankings
-for i in range(movienums - 1):
-  print(movienamekey[last[i + 1]][1])
+for i in range(len(movieInfo) - 1):
+    print(movieInfo[last[i + 1]][1])
