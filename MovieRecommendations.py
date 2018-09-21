deb6e451006b0eb9a03c8c87b13ce08bcbabf295
hiroshige.e <hiroshige.e@husky.neu.edu>
2018-09-21 00:38:28 -0400

    algorithm uses bigger data, decrease clutter

--- MovieRecommendations.py
+++ MovieRecommendations.py
@@ -1,6 +1,7 @@
 import json
 import numpy as np
 import pandas
+import time
 
 """
 TODO
@@ -27,7 +28,7 @@ def jsonify(movie_file, ratings_file):
     with open('movies.json', 'w') as file:
         file.write(json.dumps(movie_dict))
 
-    ratings_dict = {}
+    # ratings_dict = {}
     movie_id_to_critic_ratings = {}
 
     for i in range(0, len(ratings)):
@@ -35,16 +36,16 @@ def jsonify(movie_file, ratings_file):
         movie_id: int = ratings[i][1]
         rating: float = ratings[i][2]
 
-        critic: dict = ratings_dict.get(critic_id, {})
-        critic[int(movie_id)] = rating
-        ratings_dict[int(critic_id)] = critic
+        #critic: dict = ratings_dict.get(critic_id, {})
+        #critic[int(movie_id)] = rating
+        #ratings_dict[int(critic_id)] = critic
 
         critic_to_rating: dict = movie_id_to_critic_ratings.get(movie_id, {})
         critic_to_rating[int(critic_id)] = rating
         movie_id_to_critic_ratings[int(movie_id)] = critic_to_rating
 
-    with open('ratings.json', 'w') as file:
-        file.write(json.dumps(ratings_dict))
+    # with open('ratings.json', 'w') as file:
+    # file.write(json.dumps(ratings_dict))
 
     with open('movie_to_ratings.json', 'w') as file:
         file.write(json.dumps(movie_id_to_critic_ratings))
@@ -56,12 +57,13 @@ def djsonify(json_file):
         json_data = file.read()
 
     return json.loads(json_data)
-#jsonify("movies.csv", "ratings.csv")
+
 
 # title: (str) name of movie
 # movies: (dict) key: movie id, value: (dict) title and genre
 # critic_ratings: (dict) key: critic id, value: (dict) key: movie id, value: rating
-def getRecommendations(title, movies, critic_ratings, movie_to_critic_rating):
+def get_recommendations(title, movies, movie_to_critic_rating):
+    min_reviews = 20
     """
     :type title: str
     :type movies: dict
@@ -69,32 +71,39 @@ def getRecommendations(title, movies, critic_ratings, movie_to_critic_rating):
     :type movie_to_critic_rating: dict
     """
     title_to_id: dict = movies["key"]
-    movie_id = title_to_id.get(title, -1)
-    ratings_for_search = movie_to_critic_rating[movie_id]
+    movie_id = str(int(title_to_id.get(title, -1)))
+    ratings_for_search = movie_to_critic_rating.get(movie_id, -1)
     if movie_id == -1:
         raise Exception(title + " could not be found in library.")
-    if movie_id not in movie_to_critic_rating or len(ratings_for_search.keys()) < 5:
+    if movie_id not in movie_to_critic_rating or len(ratings_for_search.values()) < min_reviews:
         raise Exception("Not enough reviews for " + title + " to recommend movies")
-
+    recommendations = []
     for movie_compare, ratings_compare in movie_to_critic_rating.items():
-        if movie_compare == movie_id:
+        if movie_compare == movie_id or len(ratings_compare.keys()) < min_reviews:
             continue
         distance = 0
         critic_count = 0
-        for critic, rating in ratings_for_search:
+        for critic, rating in ratings_for_search.items():
             if critic in ratings_compare:
                 critic_count += 1
-                distance += ((ratings_for_search[critic] - ratings_compare[critic]) ** 2) ** .5
-        if (critic_count < 5):
+                distance += ((ratings_for_search[critic] - ratings_compare[critic]) ** 2)
+        if critic_count < min_reviews:
             continue
-        average_distance = distance / critic_count
+        average_distance = distance ** .5 / critic_count
+        recommendations.append((average_distance, movies[movie_compare]["title"]))
+    recommendations = sorted(recommendations, key=lambda rec: rec[0])
+    for pair in recommendations:
+        print(pair[1], pair[0])
 
 
+def main():
+    #jsonify("big_movies.csv", "big_ratings.csv")
+    movies = djsonify("movies.json")
+    movie_to_critic_rating = djsonify("movie_to_ratings.json")
+    title = input("Enter movie title: ")
+    t = time.time()
+    get_recommendations(title, movies, movie_to_critic_rating)
+    print(time.time() - t)
 
-        #for critic_rating in movie
 
-
-
-
-
-
+main()
