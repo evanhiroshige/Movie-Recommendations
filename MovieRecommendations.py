ed819af471a8989ecceb0a13089a205bb43f223e
hiroshige.e <hiroshige.e@husky.neu.edu>
2018-10-09 22:45:34 -0400

    fix algorithm

--- MovieRecommendations.py
+++ MovieRecommendations.py
@@ -8,27 +8,27 @@ import math
 # move_to_critic_ratings: (dict) key: movie id, value: (dict) key: critic id, value: rating
 def get_recommendations(title, movies, movie_to_critic_rating, genre=""):
     """
+    :param genre: genre to filter by NOTE: should change this to a list in future
     :type title: str
     :type movies: dict
     :type critic_ratings: dict
     :type movie_to_critic_rating: dict
     """
-
     min_reviews = 20
-    minimum_distance_factor = 1.25
+    minimum_distance_factor = .5
 
     title_to_id: dict = movies["key"]
     movie_id = str(int(title_to_id.get(title, -1)))
     ratings_for_search = movie_to_critic_rating.get(movie_id, -1)
-    if movie_id == '-1':
+    if movie_id == -1:
         raise Exception(title + " could not be found in library.")
-    if movie_id not in movie_to_critic_rating or len(ratings_for_search.keys()) < min_reviews:
+    if movie_id not in movie_to_critic_rating or len(ratings_for_search.items()) < min_reviews:
         raise Exception("Not enough reviews for " + title + " to recommend movies")
-    total_ratings = len(ratings_for_search.keys())
     recommendations = []
+    # for each corresponding movie
     for movie_compare, ratings_compare in movie_to_critic_rating.items():
-        if movie_compare == movie_id or genre.lower() not in movies[movie_compare]["genres"].lower() or \
-            len(ratings_compare.keys()) < min_reviews:
+        if movie_compare == movie_id or genre.lower() not in movies[movie_compare]["genres"].lower() or len(
+            ratings_compare.keys()) < min_reviews:
             continue
         distance = 0
         critic_count = 0
@@ -36,11 +36,11 @@ def get_recommendations(title, movies, movie_to_critic_rating, genre=""):
             if critic in ratings_compare:
                 critic_count += 1
                 distance += ((ratings_for_search[critic] - ratings_compare[critic]) ** 2)
-                if distance / total_ratings > minimum_distance_factor:
-                    break
-        if critic_count < min_reviews or distance / critic_count > minimum_distance_factor:
+        if critic_count < min_reviews:
             continue
-        average_distance = distance / critic_count
+        average_distance = distance ** .5 / critic_count
+        if average_distance > minimum_distance_factor:
+            continue
         recommendations.append((average_distance, movies[movie_compare]["title"],
                                 movies[movie_compare]["genres"]))
     recommendations = sorted(recommendations, key=lambda rec: rec[0])
